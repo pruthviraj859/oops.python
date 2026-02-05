9c7d466197de2e9849b17e8009b4eee5a0a038ce
Pruthviraj <pruthvirajdevadiga@gmail.com>
2026-02-05 19:04:20 +0530

    Create day4.py

---
+++ day4.py
@@ -0,0 +1,33 @@
+class Camera:
+    def __init__(self, camera_quality):
+        self.camera_quality = camera_quality
+
+    def display_camera_details(self):
+        print("Camera Quality:", self.camera_quality)
+
+
+class MusicPlayer:
+    def __init__(self, sound_quality):
+        self.sound_quality = sound_quality
+
+    def display_music_details(self):
+        print("Sound Quality:", self.sound_quality)
+
+
+class SmartPhone(Camera, MusicPlayer):
+    def __init__(self, brand, camera_quality, sound_quality):
+        self.brand = brand
+        Camera.__init__(self, camera_quality)
+        MusicPlayer.__init__(self, sound_quality)
+
+    def display_smartphone_details(self):
+        print("Brand:", self.brand)
+
+
+# Creating object
+phone = SmartPhone("Samsung", "108MP", "Dolby Atmos")
+
+# Displaying details
+phone.display_smartphone_details()
+phone.display_camera_details()
+phone.display_music_details()
