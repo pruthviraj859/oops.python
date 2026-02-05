d5cbcd3bd1d9f62e2ccbe50a8f3cc34dc5a8df6c
Pruthviraj <pruthvirajdevadiga@gmail.com>
2026-02-05 19:12:14 +0530

    Update day4.py

--- day4.py
+++ day4.py
@@ -1,5 +1,5 @@
 class Camera:
-    def __init__(self, camera_quality):
+    def __init__(self, camera_quality): 
         self.camera_quality = camera_quality
 
     def display_camera_details(self):
@@ -31,3 +31,42 @@ phone = SmartPhone("Samsung", "108MP", "Dolby Atmos")
 phone.display_smartphone_details()
 phone.display_camera_details()
 phone.display_music_details()
+class Product:
+    def __init__(self, product_name, price):
+        self.product_name = product_name
+        self.price = price
+
+    def display_product(self):
+        print("Product Name:", self.product_name)
+        print("Price:", self.price)
+
+
+class ElectronicProduct(Product):
+    def __init__(self, product_name, price, brand, warranty):
+        super().__init__(product_name, price)
+        self.brand = brand
+        self.warranty = warranty
+
+    def display_electronic_product(self):
+        print("Brand:", self.brand)
+        print("Warranty:", self.warranty)
+
+
+class MobilePhone(ElectronicProduct):
+    def __init__(self, product_name, price, brand, warranty, ram, storage):
+        super().__init__(product_name, price, brand, warranty)
+        self.ram = ram
+        self.storage = storage
+
+    def display_mobile_details(self):
+        print("RAM:", self.ram)
+        print("Storage:", self.storage)
+
+
+# Creating object
+mobile = MobilePhone("iPhone 15", 80000, "Apple", "1 Year", "8GB", "256GB")
+
+# Displaying all details
+mobile.display_product()
+mobile.display_electronic_product()
+mobile.display_mobile_details()
